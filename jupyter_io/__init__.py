aae7a8a1c7afdb110ff1f9b19692ab52bcc44607
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2024-12-10 16:35:30 +0000

    #4 Add deprecated module

--- jupyter_io/__init__.py
+++ jupyter_io/__init__.py
@@ -1,16 +1,26 @@
 __all__ = [
     # submodules
+    "deprecated",
     "save",
     # aliases
     "to_html",
     "in_notebook",
+    "savefile_in_notebook",
+    "savefig_in_notebook",
+    "savetable_in_notebook",
 ]
 __version__ = "1.0.0"
 
 
 # submodules
+from . import deprecated
 from . import save
 
 
 # aliases
+from .deprecated import (
+    savefile_in_notebook,
+    savefig_in_notebook,
+    savetable_in_notebook,
+)
 from .save import to_html, in_notebook
